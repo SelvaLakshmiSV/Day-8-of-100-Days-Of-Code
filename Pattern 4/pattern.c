6a3f5bf183e4353fade56b99401eb199774f7034
Selva Lakshmi S.V <76725996+SelvaLakshmiSV@users.noreply.github.com>
2021-04-25 02:30:08 +0530

    Pattern 4 - Print Char

---
+++ Pattern 4/pattern.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  for(int row=1;row<=5;row++,printf("\n))
+  {
+    for(int col=1;col<=5;col++)
+    {
+      printf("%d",col+64);
+          
+    }
+  }  
+  
+  
+  return 0;
+}
