0a2b2696afc51a8204bd6d053810c8efb12b1b51
Selva Lakshmi S.V <76725996+SelvaLakshmiSV@users.noreply.github.com>
2021-04-25 01:17:42 +0530

    Pattern 2
    1)This is similar to the first program 
    2)One change is printing row value instead of the star.

---
+++ Pattern 2/Pattern2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  for(int row = 1;row <= 5;row++,printf("\n"))
+  {
+    for(int col = 1;col <= 5;col++)
+    {
+      printf("%d",row);
+    }
+  }
+  return 0;
+}
+
+  
