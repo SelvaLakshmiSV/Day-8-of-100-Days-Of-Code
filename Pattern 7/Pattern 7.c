d5471a2ad4e7660d1a2249feff75353684979c79
Selva Lakshmi S.V <76725996+SelvaLakshmiSV@users.noreply.github.com>
2021-04-25 10:28:02 +0530

    Pattern 7

---
+++ Pattern 7/Pattern 7.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  for(int row=5;row>=1;row--,printf("\n"))
+  {
+    for(int col=1;col<=5;col++)
+    {
+      printf("%d ",col);
+          
+    }
+  }  
+  return 0;
+}
