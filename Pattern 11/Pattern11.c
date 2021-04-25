afeb1c00b23422528bae14429c2e31d117a76d2d
Selva Lakshmi S.V <76725996+SelvaLakshmiSV@users.noreply.github.com>
2021-04-25 11:16:12 +0530

    Pattern 11

---
+++ Pattern 11/Pattern11.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  for(int row=1;row<=5;row++,printf("\n"))
+  {
+    for(int col=1;col<=row;col++)
+    {
+      printf("%c ",col+64);
+          
+    }
+  }  
+  return 0;
+}
